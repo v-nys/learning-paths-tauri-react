3e908db8f5bb6e26834123fbc381791deba8c98e
Vincent Nys <vincent.nys@ap.be>
2023-05-12 16:02:01 +0200

    Looking good so far. Need to implement watching and stopping. Should add a component per read SVG. May not actually need routing, could just use buttons or something...

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,130 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+use std::{collections::HashMap, path::Path};
+
+use graphviz_rust::{cmd::Format, exec, printer::PrinterContext};
+use petgraph::{dot::Dot, Directed, Graph};
+use serde::{Deserialize};
+
+/* Maybe more use of references would be more idiomatic here. */
+#[derive(Deserialize)]
+struct Node {
+    id: String,
+    title: String,
+}
+
+#[derive(Deserialize)]
+struct Edge {
+    start_id: String,
+    end_id: String
+}
+
+#[derive(Deserialize)]
+struct Cluster {
+    namespace_prefix: String,
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+}
+
+#[derive(Debug)]
+enum ReadError {
+    Deserialization(serde_yaml::Error),
+    IO(std::io::Error),
+}
+
+#[derive(Debug)]
+enum SemanticError {
+    StructuralErrors, // TODO: add detail? maybe split into separate error types ReadError and InterpretationError?
+}
+
+enum Dependency {
+    Requirement(Directed),
+    Motivation(Directed),
+}
+
+#[tauri::command]
+fn read_contents(paths: &str) -> Vec<(&str, Result<Result<String, String>, String>)> {
+    eprintln!("read_contents was invoked!");
+    let paths = paths.split(";");
+    let read_results = paths.clone().map(std::fs::read_to_string);
+    let deserialized_graphs: Vec<Result<Cluster, ReadError>> = read_results
+        .map(|r| match r {
+            Ok(ref text) => serde_yaml::from_str(text).map_err(|e| ReadError::Deserialization(e)),
+            Err(e) => Err(ReadError::IO(e)),
+        })
+        .collect();
+    eprintln!("Graphs have been deserialized.");
+    let dots = deserialized_graphs.into_iter().map(|r| {
+        r.map(|c| {
+            let mut map = std::collections::HashMap::new();
+            let mut graph = Graph::new();
+            for node in c.nodes {
+                let map_key = node.id.clone();
+                if !map.contains_key(&map_key) {
+                    let idx = graph.add_node(node.id);
+                    map.insert(map_key, idx);
+                } else {
+                    // TODO: be more specific, add to vector first...
+                    return Err(SemanticError::StructuralErrors);
+                }
+            }
+            for edge in &c.edges {
+                match (map.get(&edge.start_id), map.get(&edge.end_id)) {
+                    (Some(idx1), Some(idx2)) => {
+                        graph.add_edge(*idx1, *idx2, 1);
+                    }
+                    // TODO: be more specific, add to vector first
+                    _ => return Err(SemanticError::StructuralErrors),
+                }
+            }
+            Ok(graph)
+        })
+        .map(|g| g.map(|c| format!("{:?}", Dot::new(&c))))
+    });
+    eprintln!("Dots have been generated.");
+    let svgs = dots.map(|dot_result| {
+        dot_result.map(|dot| {
+            dot.map(|ref src| {
+                let g = graphviz_rust::parse(src)
+                    .expect("Assuming petgraph generated valid dot syntax.");
+                exec(g, &mut PrinterContext::default(), vec![Format::Svg.into()])
+                    .expect("Assuming valid graph can be rendered into SVG.")
+            })
+        })
+    });
+    eprintln!("SVGs have been rendered.");
+    let serializable = svgs.map(|svg| {
+        svg.map(|ok| ok.map_err(|se| format!("{:#?}", se)))
+            .map_err(|re| format!("{:#?}", re))
+    });
+    eprintln!("Errors have been converted into strings.");
+    std::iter::zip(paths, serializable).collect()
+}
+
+#[tauri::command]
+fn associate(paths: &str) -> HashMap<&Path,Vec<&Path>> {
+    eprintln!("associate was invoked!");
+    let paths = paths.split(";").map(|p| {Path::new(p)});
+    let mut map = HashMap::new();
+    for path in paths {
+        // TODO: deal with this properly, return a Result
+        let parent = path.parent().expect("Expecting absolute path with a parent.");
+        let value = map.entry(parent).or_insert(vec![]);
+        value.push(path);
+    }
+    map
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_contents,associate])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+interface Lv2ReadResult {
+  Ok?: string,
+  Err?: string
+}
+
+interface Lv1ReadResult {
+  Ok?: Lv2ReadResult,
+  Err?: string
+}
+
+function App() {
+
+  const [paths, setPaths] = useState("");
+  const [readResults, setReadResults] = useState(new Map());
+
+  console.log(readResults);
+  
+  useEffect(() => {
+      stopWatching();
+      void (async () => {
+          if (paths.trim()) {
+              await readFileContents();
+              await startWatching();
+          }
+      })();
+  }, [paths]);
+
+  async function readFileContents() {
+      // example paths: /home/vincent/Projects/tauritest/src-tauri/test/git.yaml;/home/vincent/Projects/tauritest/src-tauri/test/got.yaml
+      let separatePaths = paths.split(";").map((p) => p.trim());
+      let svgs = await invoke('read_contents', { paths: separatePaths.join(";") });
+      let newReadResults = new Map<string,Lv1ReadResult>();
+      svgs.forEach((pair) => { newReadResults.set(pair[0], pair[1]); });
+      setReadResults(newReadResults);
+  }
+
+  function stopWatching() {
+    console.log("TODO: stop watching currently watched files.");
+  }
+
+  async function startWatching() {
+    let separatePaths = paths.split(";").map((p) => p.trim());
+    const associations = await invoke('associate', { paths: separatePaths.join(";") });
+    console.log("TODO: start watching new files and populate stopCallbacks (neither should cause re-render).");
+  }
+
+  return (
+    <div className="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <div className="row">
+        <input
+          id="files-input"
+          onChange={(e) => setPaths(e.currentTarget.value)}
+          placeholder="Enter &quot;;&quot;-separated paths"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
