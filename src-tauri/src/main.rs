6bea1c8390ab76b2974a3e19ed9290e5f7e35dc3
Vincent Nys <vincent.nys@ap.be>
2023-11-14 19:41:09 +0100

    Just more documentation per field.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -32,9 +32,9 @@ use std::{
 /// It does not need to be entirely standalone, as it can have dependencies in the form of `Edge` values.
 #[derive(Deserialize, Clone, Debug, JsonSchema)]
 struct Node {
-    /// An ID should be unique and is used to refer to it inside its `Cluster`.
+    /// An ID should be locally unique inside a `Cluster` and is used to refer to a node inside its `Cluster`.
     ///
-    /// The ID also be used to refer to the node from outside its `Cluster` if namespacing is applied.
+    /// The ID also be used to refer to the node from outside its `Cluster`, if it is preceded by the `Cluster`'s namespace prefix.
     id: String,
     /// Human-readable title for this unit of knowledge.
     ///
@@ -80,10 +80,15 @@ struct Cluster {
 #[derive(Deserialize, Clone, JsonSchema)]
 #[serde(deny_unknown_fields)]
 struct ClusterForSerialization {
+    /// This uniquely identifies the `Cluster` and allows references to its nodes to be made from inside other `Cluster`s. 
     namespace_prefix: String,
+    /// Units of information inside this `Cluster`.
     nodes: Vec<Node>,
+    /// Strict dependencies. A non-root `Node` can only be accessed if all of its dependencies of this type have been marked complete, along with one interchangeable dependency of this `Node` or of a `Node` which is strictly dependent on this `Node`.
     all_type_edges: Option<Vec<Edge>>,
+    /// Interchangeable dependencies. A non-root `Node` can only be accessed if one dependency of this type has been marked complete for this node or for a `Node` which is strictly dependent on this `Node`. Furthermore, all strict dependencies must still be marked complete.
     any_type_edges: Option<Vec<Edge>>,
+    /// IDs of `Node`s with no dependencies whatsoever, i.e. the only `Node`s which can be accessed unconditionally.
     roots: Option<Vec<String>>,
 }
 
