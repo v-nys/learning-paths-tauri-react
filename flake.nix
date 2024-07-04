003d57092adac414c8b054c9da4b6b76d3f2dea0
Vincent Nys <vincentnys@gmail.com>
2024-07-04 22:31:54 +0200

    Hope this solves the absolute paths issue.

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
               export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
               export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
               export FLAKE_DIR=$(pwd)
-              envsubst ./rust-workspace/learning-paths-tauri-react/tests/technicalinfo/pre-contents.lc.yaml > ./rust-workspace/learning-paths-tauri-react/tests/technicalinfo/contents.lc.yaml
+              envsubst -i ./rust-workspace/learning-paths-tauri-react/tests/technicalinfo/pre-contents.lc.yaml > ./rust-workspace/learning-paths-tauri-react/tests/technicalinfo/contents.lc.yaml
               npm install
             '';
         };
