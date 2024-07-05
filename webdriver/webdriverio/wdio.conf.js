00e9392092a940bd26ded57db60e650270914377
Vincent Nys <vincentnys@gmail.com>
2024-07-05 22:41:10 +0200

    Trying Selenium. Different error...

--- webdriver/webdriverio/wdio.conf.js
+++ webdriver/webdriverio/wdio.conf.js
@@ -3,9 +3,11 @@ const path = require('path')
 const { spawn, spawnSync } = require('child_process')
 
 // keep track of the `tauri-driver` child process
-let tauriDriver
+let tauriDriver;
 
 exports.config = {
+    logLevel: 'debug',
+    // runner: 'local', // added this
     specs: ['./test/specs/**/*.js'],
     maxInstances: 1,
     capabilities: [
@@ -20,13 +22,12 @@ exports.config = {
     framework: 'mocha',
     mochaOpts: {
         ui: 'bdd',
-        timeout: 5 * 60 * 1000,
+        timeout: 10 * 1000, // in millis
     },
 
     // ensure the rust project is built since we expect this binary to exist for the webdriver sessions
-    // onPrepare: () => { spawnSync('cargo', ['build', '--workspace', '--release']) },
     onPrepare: () => {
-        console.log("Rebuilding everything. This could take a while if there is no pre-existing build.");
+        console.log("Rebuilding everything...");
         spawnSync('cargo',
             ['build', '--workspace', '--release'],
             { cwd: path.join(process.env.FLAKE_DIR, "rust-workspace") });
@@ -36,13 +37,23 @@ exports.config = {
     },
 
     // ensure we are running `tauri-driver` before the session starts so that we can proxy the webdriver requests
-    beforeSession: () =>
-    (tauriDriver = spawn(
-        path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'),
-        [],
-        { stdio: [null, process.stdout, process.stderr] }
-    )),
+    beforeSession: (config, capabilities, specs) => {
+        console.log('Running tests with the following configuration:');
+        console.debug('Config:', config);
+        console.debug('Capabilities:', capabilities);
+        console.debug('Specs:', specs);
+        tauriDriver = spawn(
+            path.resolve(os.homedir(), '.cargo', 'bin', 'tauri-driver'),
+            [],
+            { stdio: [null, process.stdout, process.stderr] }
+        )
+        console.log("spawned");
+    },
 
     // clean up the `tauri-driver` process we spawned at the start of the session
-    afterSession: () => tauriDriver.kill(),
+    afterSession: () => {
+        console.log("clean up");
+        tauriDriver.kill();
+        console.log("cleaned up");
+    }
 }
