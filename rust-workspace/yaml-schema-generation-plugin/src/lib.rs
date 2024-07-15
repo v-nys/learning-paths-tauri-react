de848918f8077cc6c321a117ca04bc6ed98a2a0e
Vincent Nys <vincentnys@gmail.com>
2024-07-15 10:03:32 +0200

    Based on JSON schema validator, this looks like it will do the trick. Now to insert into overall cluster schema and to take into account additional plugin types.

--- rust-workspace/yaml-schema-generation-plugin/src/lib.rs
+++ rust-workspace/yaml-schema-generation-plugin/src/lib.rs
@@ -16,7 +16,7 @@ use serde_yaml::Value;
 use std::path::Path;
 
 pub struct YamlSchemaGenerationPlugin {
-    path: String
+    path: String,
 }
 
 #[derive(JsonSchema)]
@@ -24,8 +24,6 @@ pub struct YamlSchemaGenerationPlugin {
 pub struct PluginParameters {}
 
 impl Plugin for YamlSchemaGenerationPlugin {
-
-
     fn get_path(&self) -> &String {
         &self.path
     }
@@ -61,81 +59,70 @@ impl ClusterProcessingPlugin for YamlSchemaGenerationPlugin {
         _cluster_path: &Path,
         cluster: &domain::Cluster,
     ) -> Result<HashSet<ArtifactMapping>, anyhow::Error> {
-        let mut overall_schema = schemars::schema_for!(deserialization::ClusterForSerialization);
-        let mut plugin_schema = schemars::schema_for!(deserialization::PluginForSerialization);
-        println!(
-            "Base plugin schema:\n\n{}",
-            serde_json::to_string_pretty(&plugin_schema).unwrap()
+        let plugin_schema = schemars::schema_for!(deserialization::PluginForSerialization);
+        // TODO: don't just restrict to node plugins!
+        let plugin_paths_to_schemas: HashMap<&String, SchemaObject> = cluster
+            .node_plugins
+            .iter()
+            // this doesn't work because we have an iterator over a specific kind of container
+            // .chain(cluster.pre_zip_plugins.iter())
+            // .chain(cluster.pre_cluster_plugins.iter())
+            .map(|plugin| {
+                let params_and_schemas = plugin.get_params_schema();
+                let mut schema_for_plugin = plugin_schema.clone();
+                // cloning rather than using &mut because I'd have 2 &muts into the same struct
+                let mut required_properties_for_plugin =
+                    schema_for_plugin.schema.object().required.clone();
+                let mut properties_for_plugin =
+                    schema_for_plugin.schema.object().properties.clone();
+                params_and_schemas
+                    .iter()
+                    .for_each(|((param, required), param_schema)| {
+                        if *required {
+                            required_properties_for_plugin.insert(param.into());
+                        }
+                        let param_schema = serde_json::from_value(param_schema.clone())
+                            .expect("Assuming (de)serializating by libraries works.");
+                        properties_for_plugin.insert(param.into(), param_schema);
+                    });
+                schema_for_plugin.schema.object().required = required_properties_for_plugin;
+                schema_for_plugin.schema.object().properties = properties_for_plugin;
+                (plugin.get_path(), schema_for_plugin.schema)
+            })
+            .collect();
+
+        // can probably avoid some cloning using into_iter...
+        let conditional_schema = plugin_paths_to_schemas.iter().fold(
+            plugin_schema.schema.clone(),
+            |acc, (plugin_path, plugin_schema_object)| {
+                let mut if_clause = SchemaObject::new_ref("dummy-ref".into());
+                let mut if_clause_required = BTreeSet::new();
+                if_clause.reference = None;
+                if_clause.instance_type = Some(SingleOrVec::from(InstanceType::Object));
+                if_clause_required.insert("path".into());
+                if_clause.object().required = if_clause_required;
+                let mut if_clause_properties = BTreeMap::new();
+                let mut path_schema = SchemaObject::new_ref("dummy-ref".into());
+                path_schema.reference = None;
+                let mut path_string_validation = StringValidation::default();
+                let escaped_path_string = format!("^{}$", regex::escape(plugin_path));
+                path_string_validation.pattern = Some(escaped_path_string);
+                path_schema.string = Some(Box::new(path_string_validation));
+                if_clause_properties.insert("path".into(), Object(path_schema));
+                if_clause.object().properties = if_clause_properties;
+                let mut conditional = SchemaObject::new_ref("dummy-ref".into());
+                conditional.reference = None;
+                let conditional_subschemas = conditional.subschemas();
+                conditional_subschemas.if_schema = Some(Box::new(Object(if_clause)));
+                conditional_subschemas.then_schema =
+                    Some(Box::new(Object(plugin_schema_object.clone())));
+                conditional_subschemas.else_schema = Some(Box::new(Object(acc)));
+                conditional
+            },
         );
-        let plugin_schema_object = &mut plugin_schema.schema;
-        let subschema_validation = plugin_schema_object.subschemas();
-        let mut conditional_schemas = vec![];
-        // there is only one ClusterForSerialization schema
-        // so it should be modified through conditional clauses
-        // it can't be duplicated for every individual plugin
-        //
-        // TODO
-        // the "path" property is always required
-        // anything else is simply prohibited, *unless* we are dealing with a cluster plugin
-        // i.e. unless the path property has a value matching plugin.get_path()
-        //
-        // should be manageable, so focus on case where we are dealing with a cluster plugin
-        // instead of having multiple identical "ifs" per plugin with different "thens"
-        // there should be a single "if" for each plugin
-        // and the "then" should bundle all properties
-        // and it should prohibit any other properties
-        cluster.node_plugins.iter().for_each(|plugin| {
-            let plugin_path: &String = plugin.get_path();
-            let params_and_schemas = plugin.get_params_schema();
-            params_and_schemas.iter().for_each(
-                |((field_name, field_is_required), field_value_schema)| {
-                    // if the path matches the plugin path...
-                    let mut if_clause = SchemaObject::new_ref("dummy-ref".into());
-                    if_clause.reference = None;
-                    if_clause.instance_type = Some(SingleOrVec::from(InstanceType::Object));
-                    let mut if_clause_required = BTreeSet::new();
-                    if_clause_required.insert("path".into());
-                    if_clause.object().required = if_clause_required;
-                    let mut if_clause_properties = BTreeMap::new();
-                    let mut path_schema = SchemaObject::new_ref("dummy-ref".into());
-                    path_schema.reference = None;
-                    let mut path_string_validation = StringValidation::default();
-                    let escaped_path_string =
-                        format!("^{}$", regex::escape(plugin_path));
-                    path_string_validation.pattern = Some(escaped_path_string);
-                    path_schema.string = Some(Box::new(path_string_validation));
-                    if_clause_properties.insert("path".into(), Object(path_schema));
-                    if_clause.object().properties = if_clause_properties;
-                    // ... the parameter should be checked according to the schema ...
-                    let mut then_clause = SchemaObject::new_ref("dummy-ref".into());
-                    then_clause.reference = None;
-                    then_clause.instance_type = Some(SingleOrVec::from(InstanceType::Object));
-                    let mut then_clause_required = BTreeSet::new();
-                    if *field_is_required {
-                        then_clause_required.insert(field_name.into());
-                    }
-                    let mut then_clause_properties = BTreeMap::new();
-                    then_clause_properties.insert(
-                        field_name.into(),
-                        serde_json::from_value(field_value_schema.clone())
-                            .expect("Assuming (de)serializating by libraries works."),
-                    );
-                    then_clause.object().required = then_clause_required;
-                    then_clause.object().properties = then_clause_properties;
-                    // ... and the "if" and "then need to be joined
-                    let mut conditional = SchemaObject::new_ref("dummy-ref".into());
-                    conditional.reference = None;
-                    let conditional_subschemas = conditional.subschemas();
-                    conditional_subschemas.if_schema = Some(Box::new(Object(if_clause)));
-                    conditional_subschemas.then_schema = Some(Box::new(Object(then_clause)));
-                    conditional_schemas.push(Object(conditional));
-                },
-            );
-        });
-        subschema_validation.all_of = Some(conditional_schemas);
         println!(
             "Modified plugin schema:\n\n{}",
-            serde_json::to_string_pretty(&plugin_schema).unwrap()
+            serde_json::to_string_pretty(&conditional_schema).unwrap()
         );
         Ok(HashSet::new())
     }
@@ -143,8 +130,6 @@ impl ClusterProcessingPlugin for YamlSchemaGenerationPlugin {
 
 #[no_mangle]
 pub extern "C" fn create_plugin() -> *mut dyn ClusterProcessingPlugin {
-    let plugin = Box::new(YamlSchemaGenerationPlugin {
-        path: "".into()
-    });
+    let plugin = Box::new(YamlSchemaGenerationPlugin { path: "".into() });
     Box::into_raw(plugin)
 }
